c96bd7951bdc5e2e7723353d38a7748e786dd75d
joaomcnasc <joaomcnasc@gmail.com>
2020-03-28 11:14:19 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,39 @@
+// get - buscar, post - criar, put - alterar, delete - deletar
+//tipos de parâmetros
+/*
+query params: enviados na rota (após ?) request.query
+route params: identificar recursos (/users/id, exemplo) request.params
+request body: Corpo da requisição -> cria ou altera recursos (app.use(express.json()))
+
+*/
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use(cors());
+
+app.listen(3333);
+
+/**
+ * Entidades
+ * ONG
+ * Caso (Incident)
+ * 
+ * Funcionalidades
+ * Login de ONG
+ * Logout de ONG
+ * Cadastro de ONG
+ * Cadastrar novos casos
+ * Deletar casos
+ * Listar casos específicos de uma ONG
+ * Listar todos os casos
+ * Entrar em contato com a ONG
+ */
+
+/**
+ * migrations - controle de versão de banco de dados.
+ */
